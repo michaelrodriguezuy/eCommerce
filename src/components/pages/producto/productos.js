8d0429d896920311d02dc3853d8b028ff5675ac3
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-08-18 14:58:52 -0300

    eCommerce pronto para comenzar a codear

---
+++ src/components/pages/producto/productos.js
@@ -0,0 +1,29 @@
+export const listadoDeProductos = [
+    {
+        title: "Pelota de fútbol",
+        description: "Pelota de fútbol N°5, ideal para esos picaditos del fin de semana",
+        category: "pelotas",
+        stock: 16,
+        unit_price: 250,
+        image: "https://pixabay.com/get/g97289d9fe7e5f9253db960758b930e3c64d4f127a8acbd0c51ff5b7cad70e3314c34f1953a5675da0bd026c2a92b5e5ab775aa705a878f532936aebddf9d1dc1_1280.jpg"
+    }
+,
+    {
+        title: "Remera de fútbol",
+        description: "Remera de algodón para niño, con tecnología DryFit",
+        category: "remeras",
+        stock: 9,
+        unit_price: 320,
+        image: "https://pixabay.com/get/g98e58290094a2a59eb1ce8967e4dc019096fdbadb18a5bc023959255c04049a8d7cdf0ab8cb44c1c8df12dba2c87b17a_1280.jpg"
+    }
+ ,
+ {
+    title: "Equipo completo de fútbol americano",
+    description: "Equipo completo de fútbol americano, incluye casco, hombreras, pantalón y camiseta",
+    category: "equipos",
+    stock: 4,
+    unit_price: 650,
+    image: "https://pixabay.com/get/gbbbb3a00b915dc4dfdaa6fea6d0b6426bb8a6b692246deb53dcf8364ff1bef559df039625a9999b2f0ecf1f8fa18bf1b_1280.jpg"
+}
+ 
+]
